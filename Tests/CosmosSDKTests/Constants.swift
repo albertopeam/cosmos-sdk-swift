93e5a35a499377b0f2779c13fb1ce44569a70840
albertopeam <alberto__pa@hotmail.com>
2023-03-09 23:08:38 +0100

    added testAccountByID

--- Tests/CosmosSDKTests/Constants.swift
+++ Tests/CosmosSDKTests/Constants.swift
@@ -8,4 +8,5 @@ import Foundation
 enum Constants {
     static let atomDenom = "uatom"
     static let mainNetAddr = "cosmos196ax4vc0lwpxndu9dyhvca7jhxp70rmcfhxsrt"
+    static let mainNetAccountNumber: UInt64 = 49554
 }
--- Tests/CosmosSDKTests/cosmos_auth_v1beta1_query_tests.swift
+++ Tests/CosmosSDKTests/cosmos_auth_v1beta1_query_tests.swift
@@ -7,16 +7,38 @@ import XCTest
 @testable import CosmosSDK
 
 class cosmos_auth_v1beta1_query_tests: XCTestCase {
-    func testAccount() async throws {
+    
+    private var sut: Cosmos_Auth_V1beta1_QueryAsyncClient!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
         let channel = try GRPCChannelHelper.create(for: .main)
-        let client = Cosmos_Auth_V1beta1_QueryAsyncClient(channel: channel)
+        sut = Cosmos_Auth_V1beta1_QueryAsyncClient(channel: channel)
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        _ = sut.channel.close()
+        sut = nil
+    }
+    
+    func testAccount() async throws {
         var request = Cosmos_Auth_V1beta1_QueryAccountRequest()
         request.address = Constants.mainNetAddr
         
-        let response = try await client.account(request)
+        let response = try await sut.account(request)
                 
         let account = try Cosmos_Auth_V1beta1_BaseAccount(unpackingAny: response.account)
         XCTAssertEqual(response.account.typeURL, "/cosmos.auth.v1beta1.BaseAccount")
-        XCTAssertEqual(account.address, Constants.mainNetAddr)        
+        XCTAssertEqual(account.address, Constants.mainNetAddr)
+    }
+    
+    func testAccountByID() async throws {
+        var request = Cosmos_Auth_V1beta1_QueryAccountAddressByIDRequest()
+        request.accountID = Constants.mainNetAccountNumber
+        
+        let response = try await sut.accountAddressByID(request)
+        
+        XCTAssertEqual(response.accountAddress, Constants.mainNetAddr)
     }
 }
